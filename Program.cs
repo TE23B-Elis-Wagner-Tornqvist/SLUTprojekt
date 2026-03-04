e49d71685ad9abc7a23573468e0eeb1ce0c5e2f5
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2026-03-04 13:03:43 +0100

    slutprojekt

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Welcome to a game about smithing weapons!");
+
+string playerName = "";
+bool isValidName = false;
+int numberCheck;
+
+while (!isValidName)
+{
+    Console.Write("What is your name? ");
+    string answer = Console.ReadLine() ?? string.Empty;
+    
+    // Try to parse the input as a number
+    if (int.TryParse(answer, out numberCheck))
+    {
+        Console.WriteLine("Invalid name! Please don't enter numbers.");
+    }
+    else if (answer != "")
+    {
+        playerName = answer;
+        isValidName = true;
+    }
+    else
+    {
+        Console.WriteLine("Invalid name! Please enter a name.");
+    }
+}
+
+Console.WriteLine("Welcome, " + playerName + "!");
+
+
+Console.WriteLine(@"Which weapon do you want to smith?
+    1. Longsword
+    2. Schythe
+    3. Axe
+    4. Flail
+    ");
+
+    
+
+
+
+
+
+
+
+Console.ReadLine();
