bf451fa8ae4dbc11334d1af413786df5efc9d364
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2026-03-25 13:53:47 +0100

    ye#idk

--- Scythe.cs
+++ Scythe.cs
@@ -6,13 +6,20 @@ public class Scythe : Weapon
         sharpness += 5;
     }
 
-    
+    public int GetSharpnessSchythe()
+    {
+        return sharpness;
+    }
 
     public void HardenScythe()
     {
         hardness += 2;
     }
 
+    public int GetHardnessSchythe()
+    {
+        return hardness;
+    }
 
 
 
